634269449eb1b84e0bf72f9a973f8d8464d70922
anel-kuanyshbayeva <65378215+anel-kuanyshbayeva@users.noreply.github.com>
2020-05-18 11:03:32 -0500

    FisrtName

---
+++ Firstname.java
@@ -0,0 +1,10 @@
+public class Firstname {
+	
+	public static void main (String [] args) {
+		String name = "John";
+		System.out.println (name);
+		name = "David";
+		System.out.println(name);
+	}
+
+}

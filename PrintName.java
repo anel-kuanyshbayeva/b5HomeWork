3f1aae04b81968264366f0db85a4708ba1661bcb
anel-kuanyshbayeva <65378215+anel-kuanyshbayeva@users.noreply.github.com>
2020-05-21 12:33:11 -0500

    Add files via upload

---
+++ PrintName.java
@@ -0,0 +1,30 @@
+package Varibals;
+
+import java.util.Scanner;
+
+public class PrintName {
+	
+	public static void main (String [] args) {
+		
+		
+		
+		String letter1 = "D";
+		String letter2 = "A";
+		String letter3 = "V";
+		String letter4 = "I";
+		String letter5 = "D";
+		
+		String reverseword = ""+letter1.charAt(0)+ letter2.charAt(0) + letter3.charAt(0) + letter4.charAt(0) + letter5.charAt(0);
+		
+		System.out.println ("String name = David");
+		System.out.println (letter1);
+		System.out.println (letter2);
+		System.out.println (letter3);
+		System.out.println (letter4);
+		System.out.println (letter5);
+		
+		
+		
+	}
+
+}
